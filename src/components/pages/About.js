cfbe20235000244a96811b2d39f84521b1ed5885
TobiRino <oluwatobi.aderinokun222@gmail.com>
2022-11-06 18:57:26 +0100

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <>
+      <h1>About this App</h1>
+      <p>This projeect is an app that fetches user's repo from Github, shows their follow counts, it also rates a user's worthiness of being hired and some more interesting features, explore it to find out</p>
+      <p>Version 1.0.0</p>
+    </>
+  );
+};
+
+export default About;
